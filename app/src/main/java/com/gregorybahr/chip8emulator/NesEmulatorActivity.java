cced0d9ab522c74752311b67652e6b3874effd14
greg <greg.bahr123@gmail.com>
2017-04-17 18:29:11 -0500

    gradle files

--- app/src/main/java/com/gregorybahr/chip8emulator/NesEmulatorActivity.java
+++ app/src/main/java/com/gregorybahr/chip8emulator/NesEmulatorActivity.java
@@ -1,13 +1,21 @@
 package com.gregorybahr.chip8emulator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.gregorybahr.chip8emulator.emulators.emulatorbase.Rom;
 
 public class NesEmulatorActivity extends AppCompatActivity {
+
+    private Rom rom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nes_emulator);
+
+        rom = (Rom) getIntent().getExtras().get("rom");
+
+
     }
 }
